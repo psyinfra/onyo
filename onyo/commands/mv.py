a98e6fbd7491ab0ef97c751a73bf0fcde4442a27
Tobias Kadelka <t.kadelka@fz-juelich.de>
2022-02-24 16:40:21 +0100

    mv: use $ONYO_REPOSITORY_DIR

--- onyo/commands/mv.py
+++ onyo/commands/mv.py
@@ -10,50 +10,57 @@ from onyo.utils import *
 
 logging.basicConfig()
 logger = logging.getLogger('onyo')
+logger.setLevel(logging.INFO)
 
+def build_mv_cmd(git_path, source_filename, destination_filename, force, rename):
 
-def build_mv_cmd(source, destination, force, rename):
-    if not os.path.exists(source):
-        logger.error(source + " does not exist.")
+    if not os.path.exists( os.path.join(git_path, source_filename)):
+        logger.error(source_filename + " does not exist.")
         sys.exit(0)
-    # to look at destination path/file separately
-    if os.path.isdir(destination):
-        destination_path = destination
-        destination_filename = os.path.basename(source)
-    elif os.path.isdir(destination.replace(os.path.basename(destination), "")):
-        destination_path = destination.replace(os.path.basename(destination), "")
-        destination_filename = os.path.basename(destination)
-    # should check if folder exists. Is os.path.exists() the better function?
-    if not os.path.isdir(destination_path):
-        logger.error(destination_path + " does not exist.")
+    if os.path.basename(destination_filename) != os.path.basename(source_filename) and rename == False:
+        logger.error( os.path.basename(destination_filename) +" -> " +  os.path.basename(source_filename) #os.path.join(git_path, destination_filename) +
+                + " no renaming allowed.")
         sys.exit(0)
-    if destination_filename != os.path.basename(source) and rename == False:
-        logger.error(os.path.join(destination_path, destination_filename) +
-                " no renaming allowed.")
-        sys.exit(0)
-    if os.path.isfile(os.path.join(destination_path, destination_filename)):
+    if os.path.isfile(os.path.join(git_path, destination_filename)):
         if force == True:
-            return "git mv -f " + source + " " + os.path.join(destination_path,
-                    destination_filename)
+            return "git -C " + git_path + " mv -f " + source_filename + " " + destination_filename #os.path.join(git_path, destination_filename)
         else:
-            logger.error(os.path.join(destination_path, destination_filename) +
-                    " already exists.")
+            logger.error(os.path.join(git_path, destination_filename) + " already exists.")
             sys.exit(0)
-    return "git mv " + source + " " + os.path.join(destination_path,
-            destination_filename)
+    return "git -C " + git_path + " mv " + source_filename + " " + destination_filename
 
 
-def build_commit_cmd(source, destination):
-    return ["git commit -m", "\'move " + source + " to " + destination + "\'"]
+def build_commit_cmd(source, destination, git_directory):
+    return ["git -C " + git_directory + " commit -m", "\'move " + source + " to " + destination + "\'"]
 
 
 def mv(args):
 
     for source in args.source:
+        # set all paths
+        git_path = get_git_root(os.path.dirname(args.destination))
+        source_filename = os.path.join(os.getcwd(), source)
+        destination_filename = os.path.join(os.getcwd(), args.destination)
+        if not os.path.isfile(source_filename):
+            source_filename = os.path.join(git_path, source)
+            destination_filename = os.path.join(git_path, args.destination)
+        if not os.path.isfile(source_filename):
+            logger.error(source + " does not exist. :(")
+            sys.exit(0)
+        if not os.path.isdir(os.path.dirname(destination_filename)):
+            logger.error(source + " does not exist. :(")
+            system.exit(0)
+        # if it is just directory, but not file, add filename
+        if os.path.isdir(destination_filename):
+            destination_filename = os.path.join(destination_filename, os.path.basename(source_filename))
+
+        destination_filename = os.path.relpath(destination_filename, git_path)
+        source_filename = os.path.relpath(source_filename, git_path)
 
         # build commands
-        mv_cmd = build_mv_cmd(source, args.destination, args.force, args.rename)
-        [commit_cmd, commit_msg] = build_commit_cmd(source, args.destination)
+        mv_cmd = build_mv_cmd(git_path, source_filename, destination_filename, args.force, args.rename)
+        [commit_cmd, commit_msg] = build_commit_cmd(source_filename,
+                destination_filename, git_path)
 
         # run commands
         run_cmd(mv_cmd)
