dc3d8c354b92d568f9f46bd83a1b03cf37a75f92
Alex Waite <alex@waite.eu>
2022-09-16 16:52:15 +0200

    init: refactor
    This reduces the number of functions, adds docstrings, and greatly
    simplifies the logic.
    
    The biggest change is the use of a `skel/` directory (done in the
    previous commit). This simplifies the creation of the `.onyo` dir, as it
    no longer needs to be created programmatically, and instead can simply
    be copied in whole.
    
    Fixes #138

--- onyo/commands/init.py
+++ onyo/commands/init.py
@@ -3,59 +3,56 @@
 import logging
 import os
 import sys
-import glob
-
-from onyo.utils import (
-    build_git_add_cmd,
-    is_git_dir,
-    get_git_root,
-    run_cmd
-)
+import shutil
+from pathlib import Path
+import git
 
 logging.basicConfig()
 logger = logging.getLogger('onyo')
 
 
-def build_commit_cmd(directory):
-    return ["git -C \"" + directory + "\" commit -m", "initialize onyo repository."]
+def get_skel_dir():
+    """
+    Return the path of the skel/ dir in the onyo module directory.
+    """
+    onyo_module_dir = Path(__file__).resolve().parent.parent
+    skel = os.path.join(onyo_module_dir, 'skel/')
+    return skel
 
 
-def build_git_init_cmd(directory):
-    if is_git_dir(directory) and os.path.isdir(directory + "/.onyo"):
-        logger.error(directory + " has already a onyo configuration directory and is a git repository.")
-        sys.exit(1)
-    elif is_git_dir(directory):
-        logger.info(directory + " is already a  git repository.")
-        return None
-    return "git -C \"" + directory + "\"" + " init --initial-branch=master "
+def sanitize_dir(directory, onyo_root):
+    """
+    Check the directory for viability as an init target.
 
-
-def build_onyo_init_cmd(directory):
-    if os.path.isdir(os.path.join(directory + "/.onyo")) and not os.path.isdir(os.path.join(directory + "/.git")):
-        logger.error(directory + " has an onyo configuration directory, but " +
-                     "is not a git repository. Either delete the onyo " +
-                     "configuration directory or use git init to manually " +
-                     "initialize as git repository.")
-        sys.exit(1)
-    elif os.path.isdir(os.path.join(directory + "/.onyo")):
-        logger.error(directory + " has already an onyo configuration directory.")
-        sys.exit(1)
-    return "mkdir \"" + os.path.join(directory + "/.onyo") + "\" \"" + os.path.join(directory + "/.onyo/temp") + "\" \"" + os.path.join(directory, ".onyo/templates/") + "\" \"" + os.path.join(directory, ".onyo/validation/") + "\""
-
-
-def create_file_cmd(directory):
-    return "touch \"" + os.path.join(directory + "/.onyo/.anchor") + "\" \"" + os.path.join(directory + "/.onyo/temp/.anchor") + "\" \"" + os.path.join(directory + "/.onyo/templates/.anchor") + "\" \"" + os.path.join(directory, ".onyo/validation/.anchor") + "\""
-
-
-def prepare_arguments(directory, onyo_root):
-    if directory is None:
-        directory = onyo_root
+    Returns a normed directory on success.
+    """
+    if directory:
+        norm_dir = os.path.normpath(directory)
+        full_dir = os.path.join(onyo_root, norm_dir)
     else:
-        directory = os.path.join(onyo_root, directory)
-    if not os.path.isdir(directory):
-        logger.error("\"" + directory + "\" is no existing directory.")
+        norm_dir = os.path.normpath(onyo_root)
+        full_dir = norm_dir
+
+    # sanity checks
+    # already an .onyo repo
+    dot_onyo = os.path.join(full_dir, ".onyo")
+    if os.path.isdir(dot_onyo):
+        logger.error(dot_onyo + " already exists. Exiting.")
         sys.exit(1)
-    return directory
+
+    # target is a file, etc
+    if os.path.exists(full_dir) and not os.path.isdir(full_dir):
+        logger.error(full_dir + " exists but is not a directory. Exiting.")
+        sys.exit(1)
+
+    # make sure parent exists
+    if not os.path.isdir(full_dir):
+        parent_dir = os.path.dirname(full_dir)
+        if not os.path.isdir(parent_dir):
+            logger.error(parent_dir + " does not exist. Exiting.")
+            sys.exit(1)
+
+    return norm_dir
 
 
 def init(args, onyo_root):
@@ -70,28 +67,24 @@ def init(args, onyo_root):
     Running ``onyo init`` on an existing repository is safe. It will not
     overwrite anything; it will exit with an error.
     """
+    target_dir = sanitize_dir(args.directory, onyo_root)
+    Path(target_dir).mkdir(exist_ok=True)
 
-    # set and check path
-    directory = prepare_arguments(args.directory, onyo_root)
-    # build commands
-    git_init_command = build_git_init_cmd(directory)
-    onyo_init_command = build_onyo_init_cmd(directory)
-    create_file_command = create_file_cmd(directory)
-    git_add_command = build_git_add_cmd(directory, ".onyo/")
-    [commit_cmd, commit_msg] = build_commit_cmd(directory)
-    template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../templates/")
-    validation_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../validation/")
-    # run commands
-    if git_init_command is not None:
-        run_cmd(git_init_command)
-    run_cmd(onyo_init_command)
-    run_cmd(create_file_command)
-    os.chdir(directory)
-    os.system("onyo config history.interactive \\\"tig --follow\\\"")
-    os.system("onyo config history.non-interactive \\\"git --no-pager log --follow\\\"")
-    os.system("onyo config template.default standard")
-    run_cmd("cp -R \"" + "\" \"".join(glob.glob(os.path.join(template_path, "*"), recursive=True)) + "\" \"" + os.path.join(directory, ".onyo/templates/") + "\"")
-    run_cmd("cp -R \"" + "\" \"".join(glob.glob(os.path.join(validation_path, "*"), recursive=True)) + "\" \"" + os.path.join(directory, ".onyo/validation/") + "\"")
-    run_cmd(git_add_command)
-    run_cmd(commit_cmd, commit_msg)
-    logger.info(commit_msg + ": " + get_git_root(directory))
+    try:
+        repo = git.Repo(target_dir)
+        logger.info(target_dir + " has a git repository.")
+    except git.exc.InvalidGitRepositoryError:
+        repo = git.Repo.init(target_dir)
+
+    # populate .onyo dir
+    skel = get_skel_dir()
+    dot_onyo = os.path.join(target_dir, ".onyo")
+    shutil.copytree(skel, dot_onyo)
+
+    # add and commit
+    repo.git.add('.onyo/')
+    repo.git.commit(m='initialize onyo repository')
+
+    # print success
+    abs_dot_onyo = str(os.path.abspath(dot_onyo))
+    logger.info(f'Initialized Onyo repository in {abs_dot_onyo}')
