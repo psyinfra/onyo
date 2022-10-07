2a54a18ceca853d9980f4a4d465c6fcefcfc80c5
Alex Waite <alex@waite.eu>
2022-10-08 01:10:03 +0200

    RF: onyo invocation; add tests
    This fixes multiple bugs:
    
    - insufficient number of args to onyo would exit 0
    - args named 'config' would result in subsequent args being ignored
    - onyo -d -C /tmp (and similar) would crash with Python trace

--- onyo/main.py
+++ onyo/main.py
@@ -1,46 +1,67 @@
 #!/usr/bin/env python3
 
-from onyo import commands  # noqa: F401
-from onyo.utils import setup_parser
-
 import logging
 import sys
+
+from onyo import commands  # noqa: F401
+from onyo.utils import setup_parser
 
 logging.basicConfig()
 logger = logging.getLogger('onyo')
 logger.setLevel(logging.INFO)
 
 
+def get_subcmd_index(arglist, start=1):
+    """
+    Get the index of the subcommand from a provided list of arguments (usually sys.argv).
+
+    Returns the index on success, and None in failure.
+    """
+    # TODO: alternatively, this could use TabCompletion._argparse_to_dict()
+    # flags which accept an argument
+    flagplus = ['-C', '--onyopath']
+
+    try:
+        # find the first non-flag argument
+        nonflag = next((a for a in arglist[start:] if a[0] != '-'))
+        index = arglist.index(nonflag, start)
+    except (StopIteration, ValueError):
+        return None
+
+    # check if it's the subcommand, or just an argument to a flag
+    if arglist[index - 1] in flagplus:
+        index = get_subcmd_index(arglist, index + 1)
+
+    return index
+
+
 def main():
-    parser = setup_parser()
-
-    # NOTE: this unfortunately located hack is so "onyo config" args will pass
-    # through uninterpreted. Otherwise, anything starting with - or -- errors as
-    # an unknown option flag.
-    # nargs=argparse.REMAINDER is in theory the correct solution, but is
-    # deprecated as of Python 3.8 (due to being buggy) and did not work for me
-    # in 3.10.
+    # NOTE: this unfortunately-located-hack is to pass uninterpreted args to
+    # "onyo config".
+    # nargs=argparse.REMAINDER is supposed to do this, but did not work for our
+    # needs, and as of Python 3.8 is soft-deprecated (due to being buggy).
     # For more information, see https://docs.python.org/3.10/library/argparse.html#arguments-containing
-    if 'config' in sys.argv:
+    passthrough_subcmds = ['config']
+    subcmd_index = get_subcmd_index(sys.argv)
+    if subcmd_index and sys.argv[subcmd_index] in passthrough_subcmds:
+        # display the subcmd's --help, and don't pass it through
         if not any(x in sys.argv for x in ['-h', '--help']):
-            index = sys.argv.index('config')
-            sys.argv.insert(index + 1, '--')
+            sys.argv.insert(subcmd_index + 1, '--')
 
+    # parse the arguments
+    parser = setup_parser()
     args = parser.parse_args()
 
-    if args.onyopath:
-        onyo_root = args.onyopath
-
-    # TODO: Do onyo fsck here, test if onyo_root exists, .onyo exists, is git repo, other checks
-
+    # debugging
     if args.debug:
         logger.setLevel(logging.DEBUG)
-    if len(sys.argv) > 1 and not args.debug:
-        args.run(args, onyo_root)
-    elif len(sys.argv) > 2:
-        args.run(args, onyo_root)
+
+    # run the subcommand
+    if subcmd_index:
+        args.run(args, args.onyopath)
     else:
         parser.print_help()
+        exit(1)
 
 
 if __name__ == '__main__':
