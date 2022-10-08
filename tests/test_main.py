58b692dfd662b707454d61d4d3acc8c536b9e5bd
Alex Waite <alex@waite.eu>
2022-10-08 10:27:01 +0200

    TST: more tests for get_subcmd_index()
    Make sure that the correct index is returned when there are multiple
    instances of that value in the list.

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from onyo import main
 
 def test_get_subcmd_index_missing(helpers):
     """
-    Test all possible combinations of flags for onyo, without any subcommand.
+    All combinations of flags for onyo, without any subcommand.
     """
     for i in helpers.powerset(helpers.onyo_flags()):
         for k in product(*i):
@@ -17,11 +17,24 @@ def test_get_subcmd_index_missing(helpers):
 
 def test_get_subcmd_index_valid(helpers):
     """
-    Test all possible combinations of flags for onyo, with a subcommand.
+    All combinations of flags for onyo, with a subcommand.
     """
     for i in helpers.powerset(helpers.onyo_flags()):
         for k in product(*i):
             args = list(helpers.flatten(k))
             full_cmd = ['onyo'] + args + ['config', '--get', 'onyo.core.editor']
             idx = main.get_subcmd_index(full_cmd)
-            assert idx is full_cmd.index('config')
+            assert idx == full_cmd.index('config')
+
+
+def test_get_subcmd_index_overlap(helpers):
+    """
+    Arg values overlap with onyo or its subcommands. Borderline pathological.
+    """
+    full_cmd = ['onyo', '-C', 'onyo', '-d', 'mv', 'onyo', 'mv']
+    idx = main.get_subcmd_index(full_cmd)
+    assert idx == 4
+
+    full_cmd = ['onyo', '-C', 'mv', 'mv', 'mv', 'onyo']
+    idx = main.get_subcmd_index(full_cmd)
+    assert idx == 3
