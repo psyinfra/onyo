769d9ad90682506b5f85e86d540882461204c74c
Alex Waite <alex@waite.eu>
2022-10-22 16:54:50 +0200

    RF/TST: use repo fixture and parametrize Repo().fsck()

--- tests/lib/test_Repo_fsck.py
+++ tests/lib/test_Repo_fsck.py
@@ -1,57 +1,57 @@
 import logging
-import os
-import subprocess
 from pathlib import Path
 
 from onyo import commands  # noqa: F401
-from onyo.lib import Repo, OnyoInvalidRepoError
+from onyo.lib import OnyoInvalidRepoError
 import pytest
 
 
-def test_fsck_anchors_empty(caplog, helpers):
+#
+# Generic
+#
+variants = [
+    'anchors',
+    'asset-unique',
+    'asset-yaml',
+    'clean-tree',
+]
+@pytest.mark.parametrize('variant', variants)
+def test_fsck_empty(caplog, repo, variant):
     caplog.set_level(logging.INFO, logger='onyo')
-
-    # setup repo
-    helpers.populate_repo('fsck-anchors-empty')
-    os.chdir('fsck-anchors-empty')
-    repo = Repo('.')
 
     # test
-    repo.fsck(['anchors'])
+    repo.fsck([variant])
 
     # check log
-    # TODO: assert 'anchors' in caplog.text
+    # TODO: assert variant in caplog.text
 
 
-def test_fsck_anchors_populated(caplog, helpers):
+variants = [
+    'anchors',
+    'asset-unique',
+    'asset-yaml',
+    'clean-tree',
+]
+@pytest.mark.repo_files('README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8')
+@pytest.mark.parametrize('variant', variants)
+def test_fsck_populated(caplog, repo, variant):
     caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8']
-
-    # setup repo
-    helpers.populate_repo('fsck-anchors-populated', dirs, files)
-    os.chdir('fsck-anchors-populated')
-    repo = Repo('.')
 
     # test
-    repo.fsck(['anchors'])
+    repo.fsck([variant])
 
     # check log
-    # TODO: assert 'anchors' in caplog.text
+    # TODO: assert variant in caplog.text
 
 
-def test_fsck_anchors_missing(caplog, helpers):
+#
+# Anchors
+#
+@pytest.mark.repo_dirs('r/e/c/u/r/s/i/v/e')
+@pytest.mark.repo_files('README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8')
+def test_fsck_anchors_missing(caplog, repo):
     caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd', 'r/e/c/u/r/s/i/v/e']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8']
     anchors_to_remove = ['a/.anchor', 'r/e/c/.anchor', 'r/e/c/u/r/s/i/v/.anchor']
-
-    # setup repo
-    helpers.populate_repo('fsck-anchors-missing', dirs, files)
-    os.chdir('fsck-anchors-missing')
-    repo = Repo('.')
 
     # remove anchors
     repo._git(['rm'] + anchors_to_remove)
@@ -68,52 +68,15 @@ def test_fsck_anchors_missing(caplog, helpers):
     assert len(anchors_to_remove) == caplog.text.count('/.anchor')
 
 
-def test_fsck_unique_assets_empty(caplog, helpers):
+#
+# Asset-Unique
+#
+@pytest.mark.repo_files('README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'r/e/c/f_5', 'r/e/c/u/r/f_6', 'r/e/c/u/r/s/i/v/e/f_7')
+def test_fsck_unique_assets_conflict(caplog, repo):
     caplog.set_level(logging.INFO, logger='onyo')
-
-    # setup repo
-    helpers.populate_repo('fsck-unique-assets-empty')
-    os.chdir('fsck-unique-assets-empty')
-    repo = Repo('.')
-
-    # test
-    repo.fsck(['asset-unique'])
-
-    # check log
-    # TODO: assert 'asset-unique' in caplog.text
-
-
-def test_fsck_unique_assets_populated(caplog, helpers):
-    caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8']
-
-    # setup repo
-    helpers.populate_repo('fsck-unique-assets-populated', dirs, files)
-    os.chdir('fsck-unique-assets-populated')
-    repo = Repo('.')
-
-    # test
-    repo.fsck(['asset-unique'])
-
-    # check log
-    # TODO: assert 'asset-unique' in caplog.text
-
-
-def test_fsck_unique_assets_conflict(caplog, helpers):
-    caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd', 'r/e/c/u/r/s/i/v/e']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'r/e/c/f_5', 'r/e/c/u/r/f_6', 'r/e/c/u/r/s/i/v/e/f_7']
     assets_to_conflict = ['b/f_1', 'r/e/c/f_3', 'r/e/c/u/r/s/i/v/e/f_5']
 
-    # setup repo
-    helpers.populate_repo('fsck-unique-assets-conflict', dirs, files)
-    os.chdir('fsck-unique-assets-conflict')
-    repo = Repo('.')
-
-    # mangle files
+    # conflict some asset names
     for i in assets_to_conflict:
         Path(i).touch()
 
@@ -131,50 +94,13 @@ def test_fsck_unique_assets_conflict(caplog, helpers):
         assert 2 == caplog.text.count(Path(i).name)
 
 
-def test_fsck_yaml_empty(caplog, helpers):
+#
+# Asset-YAML
+#
+@pytest.mark.repo_files('README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'r/e/c/f_5', 'r/e/c/u/r/f_6', 'r/e/c/u/r/s/i/v/e/f_7')
+def test_fsck_yaml_invalid(caplog, repo):
     caplog.set_level(logging.INFO, logger='onyo')
-
-    # setup repo
-    helpers.populate_repo('fsck-yaml-empty')
-    os.chdir('fsck-yaml-empty')
-    repo = Repo('.')
-
-    # test
-    repo.fsck(['asset-yaml'])
-
-    # check log
-    # TODO: assert 'asset-yaml' in caplog.text
-
-
-def test_fsck_yaml_populated(caplog, helpers):
-    caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8']
-
-    # setup repo
-    helpers.populate_repo('fsck-yaml-populated', dirs, files)
-    os.chdir('fsck-yaml-populated')
-    repo = Repo('.')
-
-    # test
-    repo.fsck(['asset-yaml'])
-
-    # check log
-    # TODO: assert 'asset-yaml' in caplog.text
-
-
-def test_fsck_yaml_invalid(caplog, helpers):
-    caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd', 'r/e/c/u/r/s/i/v/e']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'r/e/c/f_5', 'r/e/c/u/r/f_6', 'r/e/c/u/r/s/i/v/e/f_7']
     files_to_mangle = ['a/f_1', 'r/e/c/f_5', 'r/e/c/u/r/s/i/v/e/f_7']
-
-    # setup repo
-    helpers.populate_repo('fsck-yaml-invalid', dirs, files)
-    os.chdir('fsck-yaml-invalid')
-    repo = Repo('.')
 
     # mangle files
     for i in files_to_mangle:
@@ -193,50 +119,13 @@ def test_fsck_yaml_invalid(caplog, helpers):
         assert i in caplog.text
 
 
-def test_fsck_clean_tree_empty(caplog, helpers):
+#
+# Clean-Tree
+#
+@pytest.mark.repo_files('README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8')
+def test_fsck_clean_tree_changed(caplog, repo):
     caplog.set_level(logging.INFO, logger='onyo')
-
-    # setup repo
-    helpers.populate_repo('fsck-clean-tree-empty')
-    os.chdir('fsck-clean-tree-empty')
-    repo = Repo('.')
-
-    # test
-    repo.fsck(['clean-tree'])
-
-    # check log
-    # TODO: assert 'clean-tree' in caplog.text
-
-
-def test_fsck_clean_tree_populated(caplog, helpers):
-    caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8']
-
-    # setup repo
-    helpers.populate_repo('fsck-clean-tree-populated', dirs, files)
-    os.chdir('fsck-clean-tree-populated')
-    repo = Repo('.')
-
-    # test
-    repo.fsck(['clean-tree'])
-
-    # check log
-    # TODO: assert 'clean-tree' in caplog.text
-
-
-def test_fsck_clean_tree_changed(caplog, helpers):
-    caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8']
     files_to_change = ['a/f_1', 'b/f_3']
-
-    # setup repo
-    helpers.populate_repo('fsck-clean-tree-changed', dirs, files)
-    os.chdir('fsck-clean-tree-changed')
-    repo = Repo('.')
 
     # change files
     for i in files_to_change:
@@ -252,24 +141,16 @@ def test_fsck_clean_tree_changed(caplog, helpers):
         assert i in caplog.text
 
 
-def test_fsck_clean_tree_staged(caplog, helpers):
+@pytest.mark.repo_files('README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8')
+def test_fsck_clean_tree_staged(caplog, repo):
     caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8']
     files_to_stage = ['a/f_1', 'b/f_3']
-
-    # setup repo
-    helpers.populate_repo('fsck-clean-tree-staged', dirs, files)
-    os.chdir('fsck-clean-tree-staged')
-    repo = Repo('.')
 
     # change and stage files
     for i in files_to_stage:
         Path(i).write_text('New contents')
 
-    ret = subprocess.run(['git', 'add'] + files_to_stage)
-    assert ret.returncode == 0
+    repo.add(files_to_stage)
 
     # test
     with pytest.raises(OnyoInvalidRepoError):
@@ -281,17 +162,10 @@ def test_fsck_clean_tree_staged(caplog, helpers):
         assert i in caplog.text
 
 
-def test_fsck_clean_tree_untracked(caplog, helpers):
+@pytest.mark.repo_files('README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8')
+def test_fsck_clean_tree_untracked(caplog, repo):
     caplog.set_level(logging.INFO, logger='onyo')
-
-    dirs = ['a', 'b', 'c', 'd']
-    files = ['README', 'a/f_1', 'a/f_2', 'b/f_3', 'b/f_4', 'c/f_5', 'c/f_6', 'd/f_7', 'd/f_8']
     files_to_be_untracked = ['LICENSE', 'd/f_9']
-
-    # setup repo
-    helpers.populate_repo('fsck-clean-tree-untracked', dirs, files)
-    os.chdir('fsck-clean-tree-untracked')
-    repo = Repo('.')
 
     # create untracked files
     for i in files_to_be_untracked:
