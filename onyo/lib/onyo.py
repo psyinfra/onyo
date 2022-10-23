78b693a7ca5f135e402529c506bd3c020a82e883
Alex Waite <alex@waite.eu>
2022-10-24 00:23:11 +0200

    NF/RF: mv -> Repo.mv()
    - move `mv` into the Repo class
    - update to take advantage of internal Repo features
    - adjust destination checks and normalize language/exceptions a bit
    - fix bugs around explicit move (file-1 -> subdir/file-1)

--- onyo/lib/onyo.py
+++ onyo/lib/onyo.py
@@ -450,6 +450,228 @@ class Repo:
         return dirs_to_create
 
     #
+    # MV
+    #
+    def mv(self, sources: Union[Iterable[Union[Path, str]], Path, str], destination: Union[Path, str], dryrun: bool = False) -> list[tuple[str, str]]:
+        """
+        Move ``source``\(s) (assets or directories) to the ``destination``
+        directory, or rename a ``source`` directory to ``destination``.
+
+        Files cannot be renamed using ``mv()``. To do so, use ``set()``.
+        """
+        if not isinstance(sources, (list, set)):
+            sources = [sources]
+        elif not isinstance(sources, list):
+            sources = list(sources)
+
+        # sanitize and validate arguments
+        src_paths = self._mv_sanitize_sources(sources)
+        dest_path = self._mv_sanitize_destination(sources, destination)
+
+        if dryrun:
+            ret = self._git(['mv', '--dry-run'] + [str(x) for x in src_paths] + [str(dest_path)])
+        else:
+            # mv and commit
+            ret = self._git(['mv'] + [str(x) for x in src_paths] + [str(dest_path)])
+            # TODO: should `mv` commit? (mkdir() does, add() doesn't)
+            # TODO: can this commit message be made more helpful?
+            # TODO: relative to self.root
+            self.commit('moved asset(s)', src_paths)
+
+        # TODO: change this to info
+        log.debug('The following will be moved:\n' +
+                  '\n'.join([str(x.relative_to(self.opdir)) for x in src_paths]))
+
+        # return a list of mv-ed assets
+        # TODO: is this relative to opdir or root? (should be opdir)
+        return [r for r in re.findall('Renaming (.*) to (.*)', ret)]
+
+    def _mv_move_mode(self, sources: list[Union[Path, str]], destination: Union[Path, str]) -> bool:
+        """
+        `mv()` can be used to either move or rename a file/directory. The mode
+        is not explicitly declared by the user, and must be inferred from the
+        arguments.
+
+        Returns True if "move" mode and False if not.
+        """
+        # can only rename one item
+        if len(sources) > 1:
+            return True
+
+        if Path(self.opdir, destination).resolve().is_dir():
+            return True
+
+        # explicitly restating the source name at the destination is a move
+        if Path(sources[0]).name == Path(destination).name and not Path(self.opdir, destination).resolve().exists():
+            return True
+
+        return False
+
+    def _mv_rename_mode(self, sources: list[Union[Path, str]], destination: Union[Path, str]) -> bool:
+        """
+        Returns True if "rename" mode and False if not.
+
+        The inverse of `move_mode()`. See its docstring for more information.
+        """
+        return not self._mv_move_mode(sources, destination)
+
+    def _mv_sanitize_destination(self, sources: list[Union[Path, str]], destination: Union[Path, str]) -> Path:
+        """
+        Perform a sanity check on the destination. This includes protected
+        paths, conflicts, and other pathological scenarios.
+
+        Returns an absolute Path on success.
+        """
+        error_path_conflict = []
+        dest_path = Path(self.opdir, destination).resolve()
+
+        """
+        Common checks
+        """
+        # protected paths
+        if is_protected_path(dest_path):
+            log.error('The following paths are protected by onyo:\n' +
+                      f'{dest_path}\n' +
+                      'Nothing was moved.')
+            raise OnyoProtectedPathError('The following paths are protected by onyo:\n' +
+                                         f'{dest_path}')
+
+        # destination cannot be a file
+        if dest_path.is_file():
+            # This intentionally raises FileExistsError rather than NotADirectoryError.
+            # It reduces the number of different exceptions that can be raised
+            # by `mv()`, and keeps the exception type unified with other similar
+            # situations (such as implicit conflict with the destination).
+            log.error(f"The destination '{dest_path}' cannot be a file.\n" +
+                      'Nothing was moved.')
+            raise FileExistsError(f"The destination '{dest_path}' cannot be a file.\n" +
+                                  'Nothing was moved.')
+
+        # check for conflicts and general insanity
+        for src in sources:
+            src_path = Path(self.opdir, src).resolve()
+            new_path = Path(dest_path, src_path.name).resolve()
+            if not dest_path.exists:
+                new_path = Path(dest_path).resolve()
+
+            # cannot rename/move into self
+            if src_path in new_path.parents:
+                log.error(f"Cannot move '{src}' into itself.\n" +
+                          "Nothing was moved.")
+                raise ValueError(f"Cannot move '{src}' into itself.\n" +
+                                 "Nothing was moved.")
+
+            # target paths cannot already exist
+            if new_path.exists():
+                error_path_conflict.append(new_path)
+                continue
+
+        if error_path_conflict:
+            log.error('The following destinations exist and would conflict:\n' +
+                      self._n_join(error_path_conflict) + '\n' +
+                      'Nothing was moved.')
+            raise FileExistsError('The following destination paths exist and would conflict.\n' +
+                                  self._n_join(error_path_conflict) + '\n' +
+                                  'Nothing was moved.')
+
+        # parent must exist
+        if not dest_path.parent.exists():
+            log.error(f"The destination '{dest_path.parent}' does not exist.\n" +
+                      "Nothing was moved.")
+            raise FileNotFoundError(f"The destination '{dest_path.parent}' does not exist.\n" +
+                                    "Nothing was moved.")
+
+        if self._mv_rename_mode(sources, destination):
+            """
+            Rename mode checks
+            """
+            log.debug("'mv' in rename mode")
+            # renaming files is not allowed
+            src_path = Path(self.opdir, sources[0]).resolve()
+            if src_path.is_file() and src_path.name != dest_path.name:
+                log.error(f"Cannot rename asset '{src_path.name}' to '{dest_path.name}'.\n" +
+                          "Use 'set()' to rename assets.\n" +
+                          "Nothing was moved.")
+                raise ValueError(f"Cannot rename asset '{src_path.name}' to '{dest_path.name}'.\n" +
+                                 "Use 'set()' to rename assets.\n" +
+                                 "Nothing was moved.")
+
+            # target cannot already exist
+            if dest_path.exists():
+                log.error(f"The destination '{dest_path}' exists and would conflict.\n" +
+                          "Nothing was moved.")
+                raise FileExistsError(f"The destination '{dest_path}' exists and would conflict.\n" +
+                                      "Nothing was moved.")
+        else:
+            """
+            Move mode checks
+            """
+            log.debug("'mv' in move mode")
+
+            # check if same name is specified as the destination
+            # (e.g. rename to same name is a move)
+            if src_path.name != dest_path.name:
+                # dest must exist
+                if not dest_path.exists():
+                    log.error(f"The destination '{destination}' does not exist.\n" +
+                              "Nothing was moved.")
+                    raise FileNotFoundError(f"The destination '{destination}' does not exist.\n" +
+                                            "Nothing was moved.")
+
+            # cannot move onto self
+            if src_path.is_file() and dest_path.is_file() and src_path.samefile(dest_path):
+                log.error(f"Cannot move '{src}' onto itself.\n" +
+                          "Nothing was moved.")
+                raise FileExistsError(f"Cannot move '{src}' onto itself.\n" +
+                                      "Nothing was moved.")
+
+        return dest_path
+
+    def _mv_sanitize_sources(self, sources: list[Union[Path, str]]) -> list[Path]:
+        """
+        Check and normalize a list of paths. If any do not exist, or are
+        protected paths (.anchor, .git, .onyo), then an exception will be
+        raised.
+
+        Returns a list of absolute Paths.
+        """
+        paths_to_mv = []
+        error_path_absent = []
+        error_path_protected = []
+
+        # validate sources
+        for src in sources:
+            full_path = Path(self.opdir, src).resolve()
+
+            # paths must exist
+            if not full_path.exists():
+                error_path_absent.append(src)
+                continue
+
+            # protected paths
+            if is_protected_path(full_path):
+                error_path_protected.append(src)
+                continue
+
+            paths_to_mv.append(full_path)
+
+        if error_path_absent:
+            log.error('The following source paths do not exist:\n' +
+                      self._n_join(error_path_absent) + '\n' +
+                      'Nothing was moved.')
+            raise FileNotFoundError('The following paths do not exist:\n' +
+                                    self._n_join(error_path_absent))
+
+        if error_path_protected:
+            log.error('The following paths are protected by onyo:\n' +
+                      self._n_join(error_path_protected) + '\n' +
+                      'Nothing was moved.')
+            raise OnyoProtectedPathError('The following paths are protected by onyo:\n' +
+                                         self._n_join(error_path_protected))
+
+        return paths_to_mv
+
+    #
     # RM
     #
     def rm(self, paths: Union[Iterable[Union[Path, str]], Path, str], dryrun: bool = False) -> list[str]:
