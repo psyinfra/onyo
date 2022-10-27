967bb44e30abcf0b479524c4cec10dd573bec187
Alex Waite <alex@waite.eu>
2022-10-28 00:22:25 +0200

    TST/RF: update utils tests to use repo fixture

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,50 +5,41 @@ import pytest
 from onyo import utils
 
 
-def test_onyo_init():
-    ret = subprocess.run(["onyo", "init"])
-    assert ret.returncode == 0
-
-
-def test_get_config_value_git():
+def test_get_config_value_git(repo):
     ret = subprocess.run(["git", "config", "onyo.test.get-git", "get-git-test"],
                          capture_output=True, text=True)
     assert ret.returncode == 0
     assert 'get-git =' in Path('.git/config').read_text()
     assert '= get-git-test' in Path('.git/config').read_text()
 
-    onyo_root = './'
-    assert utils.get_config_value('onyo.test.get-git', onyo_root) == 'get-git-test'
+    assert utils.get_config_value('onyo.test.get-git', repo.root) == 'get-git-test'
 
 
-def test_get_config_value_onyo():
+def test_get_config_value_onyo(repo):
     ret = subprocess.run(["onyo", "config", "onyo.test.get-onyo", "get-onyo-test"],
                          capture_output=True, text=True)
     assert ret.returncode == 0
     assert 'get-onyo =' in Path('.onyo/config').read_text()
     assert '= get-onyo-test' in Path('.onyo/config').read_text()
 
-    onyo_root = './'
-    assert utils.get_config_value('onyo.test.get-onyo', onyo_root) == 'get-onyo-test'
+    assert utils.get_config_value('onyo.test.get-onyo', repo.root) == 'get-onyo-test'
 
 
-def test_generate_faux_serials():
+def test_generate_faux_serials(repo):
     """
     Test the creation of unique serial numbers with the default length through
     calling the generate_faux_serial() function multiple times and comparing the
     generated faux serial numbers.
     """
-    onyo_root = './'
-
-    # technically, creating multiple faux serial numbers without actually
-    # creating assets might lead to duplicates. In practice, this should not
-    # happen if they are sufficiently long and random
-    faux = [utils.generate_faux_serial(onyo_root) for x in range(0, 100)]
+    # creating multiple faux serial numbers without actually creating assets
+    # might lead to duplicates. In practice, this should not happen if they are
+    # sufficiently long and random
+    faux = [utils.generate_faux_serial(repo.root) for x in range(0, 100)]
     assert len(faux) == len(set(faux))
     assert len(faux) > 0
 
 
-def test_length_range_of_generate_faux_serials():
+def test_length_range_of_generate_faux_serials(repo):
     """
     Create many faux serial numbers with different lengths over the range of
     allowed faux serial number lengths to test that the function returns faux
@@ -56,26 +47,24 @@ def test_length_range_of_generate_faux_serials():
     to verify that the appropriate error gets thrown. Because of the
     different lengths, they should all be different.
     """
-    onyo_root = './'
-
-    # test that faux serial numbers must have at least length 1 and the function
-    # raises the correct ValueError:
+    # faux serial numbers must have at least length 1 and the function raises
+    # ValueError:
     with pytest.raises(ValueError) as ret:
-        utils.generate_faux_serial(onyo_root, faux_length=0)
+        utils.generate_faux_serial(repo.root, faux_length=0)
     assert ret.type == ValueError
 
-    # test that faux serial numbers can't be requested longer than 37 and the
-    # function raises the correct ValueError:
+    # faux serial numbers can't be requested longer than 37 and the function
+    # raises ValueError:
     with pytest.raises(ValueError) as ret:
-        utils.generate_faux_serial(onyo_root, faux_length=38)
+        utils.generate_faux_serial(repo.root, faux_length=38)
     assert ret.type == ValueError
 
-    faux = [utils.generate_faux_serial(onyo_root, x) for x in range(1, 37)]
+    faux = [utils.generate_faux_serial(repo.root, x) for x in range(1, 37)]
     assert len(faux) == len(set(faux))
     assert len(faux) > 0
 
 
-def test_is_protected_path():
+def test_is_protected_path(repo):
     # simple
     assert utils.is_protected_path('.anchor')
     assert utils.is_protected_path('.git')
