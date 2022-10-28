d7a4beafd03540da1fd1f312f6ade70e9ceefce8
Alex Waite <alex@waite.eu>
2022-10-28 08:24:07 +0200

    RF: modernize `tree`
    This is just a quick and dirty modernization, to get it working without
    run_cmd().

--- onyo/commands/tree.py
+++ onyo/commands/tree.py
@@ -1,54 +1,43 @@
 import logging
 import os
 import sys
+from pathlib import Path
 
-from onyo.utils import (
-    run_cmd
-)
 from onyo.lib import Repo, OnyoInvalidRepoError
 
 logging.basicConfig()
 log = logging.getLogger('onyo')
 
 
-def build_tree_cmd(directory):
-    if not os.path.isdir(directory):
-        log.error(directory + " does not exist.")
-        sys.exit(1)
-    return "tree \"" + directory + "\""
-
-
-def prepare_arguments(sources, opdir):
-    problem_str = ""
-    list_of_sources = []
-    # just a single path?
-    single_source = "".join(sources)
-    if os.path.isdir(single_source):
-        return [single_source]
-    elif os.path.isdir(os.path.join(opdir, single_source)):
-        return [os.path.join(opdir, single_source)]
-    # build paths
-    for source in sources:
-        current_source = source
-        if not os.path.exists(current_source):
-            current_source = os.path.join(opdir, source)
-        # check if path exists
-        if not os.path.exists(current_source):
-            problem_str = problem_str + "\n" + source + " does not exist."
-        elif not os.path.isdir(current_source):
-            problem_str = problem_str + "\n" + source + " is not a directory."
-        else:
-            list_of_sources.append(current_source)
-    if problem_str != "":
-        log.error(problem_str)
-        sys.exit(1)
-    return list_of_sources
-
-
-def tree(args, opdir):
+def sanitize_directories(directories: list, opdir: str) -> str:
     """
-    List the assets and directories in ``directory`` using the ``tree``
-    program.
+    Check a list of directories. If any do not exist or are a file, and error
+    will be printed.
+
+    Returns a string of the valid directories relative to opdir.
+    """
+    dirs = []
+    error_path = []
+
+    for d in directories:
+        full_path = Path(opdir, d)
+
+        if full_path.is_dir():
+            dirs.append(full_path.relative_to(opdir))
+        else:
+            error_path.append(d)
+
+    if error_path:
+        print('The following paths are not directories:\n' + '\n'.join(error_path),
+              file=sys.stderr)
+        sys.exit(1)
+
+    return ' '.join(str(x) for x in dirs)
+
+
+def tree(args, opdir: str) -> None:
+    """
+    List the assets and directories in ``directory`` using ``tree``.
     """
     try:
         repo = Repo(opdir)
@@ -56,10 +45,23 @@ def tree(args, opdir):
     except OnyoInvalidRepoError:
         sys.exit(1)
 
-    # check sources
-    list_of_sources = prepare_arguments(args.directory, opdir)
-    # build and run commands
-    for source in list_of_sources:
-        tree_command = build_tree_cmd(source)
-        output = run_cmd(tree_command)
-        print(output)
+    # sanitize the paths
+    dirs = sanitize_directories(args.directory, opdir)
+
+    # run it
+    status = int()
+    orig_cwd = os.getcwd()
+    try:
+        os.chdir(opdir)
+        status = os.system(f"tree {dirs}")
+    except:  # noqa: E722
+        pass
+    finally:
+        os.chdir(orig_cwd)
+
+    # covert the return status into a return code
+    returncode = os.waitstatus_to_exitcode(status)
+
+    # bubble up error retval
+    if returncode != 0:
+        exit(returncode)
