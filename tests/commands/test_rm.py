065665263130dfec609b0afb29a6d65b977af75c
Tobias Kadelka <t.kadelka@fz-juelich.de>
2023-02-24 12:51:20 +0100

    TST: modernize `test_rm.py`
    Adds pyre type checking, which was still missing here.
    Adds "*assets", which makes tests more flexible to modify later,
    especially with `pytest.mark.repo_files`.

--- tests/commands/test_rm.py
+++ tests/commands/test_rm.py
@@ -17,83 +17,101 @@ assets = ['laptop_apple_macbookpro.0',
 #
 # FLAGS
 #
-@pytest.mark.repo_files('laptop_apple_macbook.abc123')
-def test_rm_interactive_missing_y(repo):
+@pytest.mark.repo_files(*assets)
+def test_rm_interactive_missing_y(repo: Repo) -> None:
     """
     Default mode is interactive. It requires a "y" to approve.
     """
-    ret = subprocess.run(['onyo', 'rm', 'laptop_apple_macbook.abc123'], capture_output=True, text=True)
+    ret = subprocess.run(['onyo', 'rm', *assets], capture_output=True, text=True)
     assert ret.returncode == 1
     assert "Delete assets? (y/N) " in ret.stdout
     assert ret.stderr
 
-    assert Path('laptop_apple_macbook.abc123').exists()
+    # verify no changes were made and the repository is in a clean state
+    for asset in assets:
+        assert Path(asset).exists()
+    repo.fsck()
 
 
-@pytest.mark.repo_files('laptop_apple_macbook.abc123')
-def test_rm_interactive_abort(repo):
+@pytest.mark.repo_files(*assets)
+def test_rm_interactive_abort(repo: Repo) -> None:
     """
     Default mode is interactive. Provide the "n" to abort.
     """
-    ret = subprocess.run(['onyo', 'rm', 'laptop_apple_macbook.abc123'], input='n', capture_output=True, text=True)
+    ret = subprocess.run(['onyo', 'rm', *assets], input='n', capture_output=True, text=True)
     assert ret.returncode == 0
     assert "Delete assets? (y/N) " in ret.stdout
     assert not ret.stderr
 
-    assert Path('laptop_apple_macbook.abc123').exists()
+    # verify no changes were made and the repository is in a clean state
+    for asset in assets:
+        assert Path(asset).exists()
+    repo.fsck()
 
 
-@pytest.mark.repo_files('laptop_apple_macbook.abc123')
-def test_rm_interactive(repo):
+@pytest.mark.repo_files(*assets)
+@pytest.mark.parametrize('asset', assets)
+def test_rm_interactive(repo: Repo, asset: str) -> None:
     """
     Default mode is interactive. Provide the "y" to approve.
     """
-    ret = subprocess.run(['onyo', 'rm', 'laptop_apple_macbook.abc123'], input='y', capture_output=True, text=True)
+    ret = subprocess.run(['onyo', 'rm', asset], input='y', capture_output=True, text=True)
     assert ret.returncode == 0
     assert "Delete assets? (y/N) " in ret.stdout
     assert not ret.stderr
 
-    assert not Path('laptop_apple_macbook.abc123').exists()
+    # verify deleting was successful and the repository is in a clean state
+    assert not Path(asset).exists()
+    repo.fsck()
 
 
-@pytest.mark.repo_files('laptop_apple_macbook.abc123')
-def test_rm_quiet_missing_yes(repo):
+@pytest.mark.repo_files(*assets)
+def test_rm_quiet_missing_yes(repo: Repo) -> None:
     """
     ``--quiet`` requires ``--yes``
     """
-    ret = subprocess.run(['onyo', 'rm', '--quiet', 'laptop_apple_macbook.abc123'], capture_output=True, text=True)
+    ret = subprocess.run(['onyo', 'rm', '--quiet', *assets], capture_output=True, text=True)
     assert ret.returncode == 1
     assert not ret.stdout
     assert ret.stderr
 
-    assert Path('laptop_apple_macbook.abc123').exists()
+    # verify no changes were made and the repository is in a clean state
+    for asset in assets:
+        assert Path(asset).exists()
+    repo.fsck()
 
 
-@pytest.mark.repo_files('laptop_apple_macbook.abc123')
-def test_rm_quiet(repo):
+@pytest.mark.repo_files(*assets)
+def test_rm_quiet(repo: Repo) -> None:
     """
     ``--quiet`` requires ``--yes``
     """
-    ret = subprocess.run(['onyo', 'rm', '--yes', '--quiet', 'laptop_apple_macbook.abc123'], capture_output=True, text=True)
+    ret = subprocess.run(['onyo', 'rm', '--yes', '--quiet', *assets], capture_output=True, text=True)
     assert ret.returncode == 0
     assert not ret.stdout
     assert not ret.stderr
 
-    assert not Path('laptop_apple_macbook.abc123').exists()
+    # verify deleting was successful and the repository is in a clean state
+    for asset in assets:
+        assert not Path(asset).exists()
+    repo.fsck()
 
 
-@pytest.mark.repo_files('laptop_apple_macbook.abc123')
-def test_rm_yes(repo):
+@pytest.mark.repo_files(*assets)
+@pytest.mark.parametrize('asset', assets)
+def test_rm_yes(repo: Repo, asset: str) -> None:
     """
     --yes removes any prompts and auto-approves the deletion.
     """
-    ret = subprocess.run(['onyo', 'rm', '--yes', 'laptop_apple_macbook.abc123'], capture_output=True, text=True)
+    ret = subprocess.run(['onyo', 'rm', '--yes', asset], capture_output=True, text=True)
     assert ret.returncode == 0
     assert "The following will be deleted:" in ret.stdout
     assert "Remove assets? (y/N) " not in ret.stdout
     assert not ret.stderr
 
-    assert not Path('laptop_apple_macbook.abc123').exists()
+    # verify deleting was successful and the repository is in a clean state
+    assert not Path(asset).exists()
+    repo.fsck()
 
 
 @pytest.mark.repo_files(*assets)
