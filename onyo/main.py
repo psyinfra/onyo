3711e8cfcdff7fd1dda233bec1002b21c48dbfd5
Tobias Kadelka <t.kadelka@fz-juelich.de>
2023-03-31 11:48:37 +0200

    add pyre to main.py

--- onyo/main.py
+++ onyo/main.py
@@ -83,35 +83,35 @@ def parse_key_values(string):
     return results
 
 
-def directory(string):
+def directory(string: str):
     """
     A no-op type-check for ArgParse. Used to hint for shell tab-completion.
     """
     return string
 
 
-def file(string):
+def file(string: str):
     """
     A no-op type-check for ArgParse. Used to hint for shell tab-completion.
     """
     return string
 
 
-def git_config(string):
+def git_config(string: str):
     """
     A no-op type-check for ArgParse. Used to hint for shell tab-completion.
     """
     return string
 
 
-def path(string):
+def path(string: str):
     """
     A no-op type-check for ArgParse. Used to hint for shell tab-completion.
     """
     return string
 
 
-def template(string):
+def template(string: str):
     """
     A no-op type-check for ArgParse. Used to hint for shell tab-completion.
     """
