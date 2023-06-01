e6f3036abf578619ce9e599851f2a622b93687ad
Tobias Kadelka <t.kadelka@fz-juelich.de>
2023-06-01 10:46:04 +0200

    docs/get: add information from #18
    This still is not a full overhaul, I just collect/connect
    so as to have a good/smooth starting point when re-working
    the documentation of Onyo and its commands.

--- onyo/commands/get.py
+++ onyo/commands/get.py
@@ -17,13 +17,18 @@ log = logging.getLogger('onyo')
 def get(args: argparse.Namespace) -> None:
     """
     Return matching asset(s) and values corresponding to the requested key(s).
+
     If no key(s) are given, the pseudo-keys are returned instead.
+    If no ``asset`` or ``directory`` is specified, the current working directory
+    is used.
 
     Filters can make use of pseudo-keys (i.e., keys for which the values are
     only stored in the asset name). Values of the dictionary or list type, as
     well as assets missing a value can be referenced as '<dict>', '<list>',
     or '<unset>' instead of their contents, respectively. If a requested key
     does not exist, its output is displayed as '<unset>'.
+
+    The ``value`` of filters can be a string or a Python regular expression.
 
     By default, the returned assets are sorted by their paths.
     """
