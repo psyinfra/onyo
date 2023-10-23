e8de535ddb731a2a931ba8d14aa8abc218de3586
Tobias Kadelka <t.kadelka@fz-juelich.de>
2023-10-23 12:33:19 +0200

    demo: update command calls to use new syntax
    Since the refactoring, the demo fails because the syntax, especially
    of `onyo new` did change.

--- demo/generate_demo_repo.sh
+++ demo/generate_demo_repo.sh
@@ -96,18 +96,19 @@ onyo --yes mkdir repair
 onyo -y new --tsv "${SCRIPT_DIR}/inventory.tsv"
 
 # add a set of newly bought assets
-onyo -y new --keys RAM=8GB display=13.3 --path warehouse/laptop_apple_macbook.9r32he
-onyo -y new --keys RAM=8GB display=13.3 --path warehouse/laptop_apple_macbook.9r5qlk
-onyo -y new --keys RAM=8GB display=14.6 --path warehouse/laptop_lenovo_thinkpad.owh8e2
-onyo -y new --keys RAM=8GB display=14.6 --path warehouse/laptop_lenovo_thinkpad.iu7h6d
-onyo -y new --keys RAM=8GB display=12.4 touchscreen=yes --path warehouse/laptop_microsoft_surface.oq782j
+onyo -y new --keys type=laptop make=apple model=macbook serial=9r32he RAM=8GB display=13.3 --path warehouse/
+onyo -y new --keys type=laptop make=apple model=macbook serial=9r5qlk RAM=8GB display=13.3 --path warehouse/
+onyo -y new --keys type=laptop make=lenovo model=thinkpad serial=owh8e2 RAM=8GB display=14.6 --path warehouse/
+onyo -y new --keys type=laptop make=lenovo model=thinkpad serial=iu7h6d RAM=8GB display=14.6 --path warehouse/
+onyo -y new --keys type=laptop make=microsoft model=surface serial=oq782j RAM=8GB display=12.4 touchscreen=yes --path warehouse/
+
 # NOTE: headphones normally do not have a serial number, and thus a faux serial
 # would be generated (e.g. headphones_JBL_pro.faux). However, for the sake of a
 # reproducible demo, explicit serials are specified.
-onyo -y new --path warehouse/headphones_apple_airpods.7h8f04
-onyo -y new --path warehouse/headphones_JBL_pro.325gtt
-onyo -y new --path warehouse/headphones_JBL_pro.e98t2p
-onyo -y new --path warehouse/headphones_JBL_pro.ph9527
+onyo -y new --keys type=headphones make=apple model=airpods serial=7h8f04 --path warehouse/
+onyo -y new --keys type=headphones make=JBL model=pro serial=325gtt --path warehouse/
+onyo -y new --keys type=headphones make=JBL model=pro serial=e98t2p --path warehouse/
+onyo -y new --keys type=headphones make=JBL model=pro serial=ph9527 --path warehouse/
 
 # one of the headphones was added by accident; remove it.
 onyo -y rm warehouse/headphones_JBL_pro.ph9527
@@ -133,14 +134,14 @@ onyo -y set --keys USB_A=2 --filter type=laptop
 onyo -y set --keys USB_A=2 USB_C=1 --filter model=macbook
 
 # add three newly purchased laptops; shell brace-expansion can be very useful
-onyo -y new --keys RAM=8GB display=13.3 USB_A=2 USB_C=1 \
-    --path warehouse/laptop_apple_macbook.{uef82b3,9il2b4,73b2cn}
+onyo -y new --keys type=laptop make=apple model=macbook serial={uef82b3,9il2b4,73b2cn} RAM=8GB display=13.3 USB_A=2 USB_C=1 \
+    --path warehouse/
 
 # Bingo Bob was hired; and new hardware was purchased for him
 onyo --yes mkdir "accounting/Bingo Bob"
-onyo -y new --keys display=22.0 --path warehouse/monitor_dell_PH123.86JZho
-onyo -y new --keys RAM=8GB display=13.3 USB_A=2 --path warehouse/laptop_apple_macbook.oiw629
-onyo -y new --path warehouse/headphones_apple_airpods.uzl8e1
+onyo -y new --keys type=monitor make=dell model=PH123 serial=86JZho display=22.0 --path warehouse/
+onyo -y new --keys type=laptop make=apple model=macbook serial=oiw629 RAM=8GB display=13.3 USB_A=2 --path warehouse/
+onyo -y new --keys type=headphones make=apple model=airpods serial=uzl8e1 --path warehouse/
 onyo -y mv warehouse/monitor_dell_PH123.86JZho warehouse/laptop_apple_macbook.oiw629 warehouse/headphones_apple_airpods.uzl8e1 "accounting/Bingo Bob"
 
 # the broken laptop has been repaired (bad RAM, which has also been increased)
@@ -155,7 +156,7 @@ onyo -y mv warehouse/laptop_apple_macbook.uef82b3 ethics/Max\ Mustermann/
 onyo -y mkdir "management"
 onyo -y mv "ethics/Max Mustermann" management
 onyo -y mkdir "management/Alice Wonder"
-onyo -y new --keys RAM=8GB display=13.3 USB_A=2 --path "management/Alice Wonder/laptop_apple_macbook.83hd0"
+onyo -y new --keys type=laptop make=apple model=macbook serial=83hd0 RAM=8GB display=13.3 USB_A=2 --path "management/Alice Wonder/"
 
 # Theo joins; assign them a laptop from the warehouse
 onyo -y mkdir "ethics/Theo Turtle"
@@ -165,9 +166,7 @@ onyo -y mv warehouse/laptop_lenovo_thinkpad.owh8e2 "ethics/Theo Turtle"
 onyo -y mv management/Max\ Mustermann/* warehouse
 onyo -y rm "management/Max Mustermann"
 
-# test the validity of the inventory's state
-onyo fsck
-
+# TODO: add "onyo fsck"
 # TODO: compare
 # git log
 # assert
