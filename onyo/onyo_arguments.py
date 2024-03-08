6affb6cfc798105489c9c66ae0e8c54692b3b346
Alex Waite <alex@waite.eu>
2024-03-08 15:23:42 +0100

    DOC: improve formatting of help text for top-level arguments

--- onyo/onyo_arguments.py
+++ onyo/onyo_arguments.py
@@ -10,36 +10,48 @@ args_onyo = {
         required=False,
         default=Path.cwd(),
         type=directory,
-        help='Run Onyo commands from inside of DIR'),
+        help="""
+            Run Onyo from DIR instead of the current working directory.
+        """
+    ),
 
     'debug': dict(
         args=('-d', '--debug'),
         required=False,
         default=False,
         action='store_true',
-        help='Enable debug logging'),
+        help="""
+            Enable debug logging.
+        """
+    ),
 
     'version': dict(
         args=('-v', '--version'),
         action='version',
         version='%(prog)s {version}'.format(version=__version__),
-        help="Print onyo's version and exit"),
+        help="""
+            Print Onyo's version and exit.
+        """
+    ),
 
     'quiet': dict(
         args=('-q', '--quiet'),
         required=False,
         default=False,
         action='store_true',
-        help=(
-            'Silence messages printed to stdout. Does not suppress interactive '
-            'editors. Requires the --yes flag')),
+        help="""
+            Silence messages printed to stdout; does not suppress interactive
+            editors. Requires the ``--yes`` flag.
+        """
+    ),
 
     'yes': dict(
         args=('-y', '--yes'),
         required=False,
         default=False,
         action='store_true',
-        help=(
-            'Respond "yes" to any prompts. The --yes flag is required to use '
-            '--quiet')),
+        help="""
+            Respond "yes" to any prompts.
+        """
+    ),
 }
