1e55ad90b4c1ae53573ff8471f2d29e37a8b28e9
Alex Waite <alex@waite.eu>
2024-03-09 09:13:16 +0100

    DOC: improve help text for `onyo init`

--- onyo/commands/init.py
+++ onyo/commands/init.py
@@ -14,22 +14,33 @@ args_init = {
         metavar='DIR',
         nargs='?',
         type=directory,
-        help='Initialize DIR as an onyo repository')
+        help="""
+            Initialize DIR as an Onyo repository.
+        """
+    )
 }
 
 
 def init(args: argparse.Namespace) -> None:
     """
-    Initialize an Onyo repository. The directory will be initialized as a git
-    repository (if it is not one already), the ``.onyo/`` directory created and
-    populated with config files, templates, etc. If the directory specified does
-    not exist, it will be created. Everything will be committed.
+    Initialize an Onyo repository.
 
-    The current working directory will be initialized if neither ``directory``
-    nor the ``onyo -C DIR`` option are specified.
+    The current working directory will be initialized if neither ``DIR`` nor the
+    ``onyo -C DIR`` flag are specified. If the target directory does not exist,
+    it will be created.
 
-    Running ``onyo init`` on an existing repository is safe. It will not
-    overwrite anything.
+    Initialization steps are:
+
+        * create the target directory (if it does not exist)
+        * initialize as a git repository (if it is not one already)
+        * create the ``.onyo/`` directory, populate its contents, and commit
+
+    Running ``onyo init`` on non-empty directories and git repositories is
+    allowed. Only the ``.onyo`` directory will be committed. All other contents
+    will be left in their state.
+
+    Running ``onyo init`` repeatedly on a repository will not alter the
+    contents, and will exit with an error.
     """
     target_dir = Path(args.directory).resolve() if args.directory else Path.cwd()
     OnyoRepo(target_dir, init=True)
