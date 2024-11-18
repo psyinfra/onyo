73d54523e45801534b2dd7bed933c8ffa1b2d20e
Alex Waite <alex@waite.eu>
2024-11-18 12:47:24 +0100

    TST: add Faker fixture ("fake")
    Will be used for upcoming benchmark tests, but will also be useful
    across tests in general.
    
    This also makes them available across scopes. Unsure about the utility
    of this, given the large number of bugs I've encountered with fixture
    scope and parametrization, but I wanted this in the repo history so we
    can refer to it.
    
    fix #254

--- onyo/conftest.py
+++ onyo/conftest.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 import os
 import subprocess
 from collections.abc import Iterable
+from contextlib import contextmanager
 from itertools import chain, combinations
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
 from _pytest.mark.structures import MarkDecorator
+from faker.providers import python
 
 from onyo.lib.git import GitRepo
 from onyo.lib.inventory import Inventory
@@ -258,3 +260,189 @@ def set_ui(request) -> None:
         ui.set_yes(m.args[0].get('yes', False))
         ui.set_quiet(m.args[0].get('quiet', False))
         ui.set_debug(m.args[0].get('debug', False))
+
+
+class OnyoProvider(python.Provider):
+    r"""
+    Faker Provider for Onyo.
+    """
+    def onyo_asset_dicts(self,
+                         num: int = 1,
+                         override: dict | None = None) -> Generator[dict, None, None]:
+        r"""
+        Yield asset directories suitable for populating realistic assets.
+        """
+        if override is None:
+            override = {}
+
+        for _ in range(num):
+            yield {
+                'type': next(self.onyo_types()),
+                'make': next(self.onyo_manufacturers()),
+                'model': { 'name': self.numerify(text='Wizbang %##!') },
+                'serial': self.pystr(min_chars=15),
+                'keyboard': next(self.onyo_keyboards()),
+                'display': {
+                    'size': self.numerify(text='%#'),
+                    'resolution': self.numerify(text='%##! x %##!'),
+                    'hz': self.numerify(text='%#!'),
+                },
+                'RAM': self.numerify(text='%#!!G'),
+                'CPU': {
+                    'vendor': next(self.onyo_cpu_vendors()),
+                    'cores': self.numerify(text='%#!'),
+                    'arch': next(self.onyo_cpu_archs()),
+                    'model': self.numerify(text='Speedy % %%##Z'),
+                },
+                'disk': { 'size': self.numerify(text='%#!T') },
+            } | override
+
+
+    def onyo_cpu_archs(self,
+                       num: int = 1) -> Generator[str, None, None]:
+        r"""
+        Yield CPU architectures.
+        """
+        cpu_archs = (
+            'aarch64',
+            'amd64',
+            'ppc64',
+            'x86',
+        )
+        yield self.random_element(elements=cpu_archs)
+
+
+    def onyo_cpu_vendors(self,
+                         num: int = 1) -> Generator[str, None, None]:
+        r"""
+        Yield CPU vendors.
+        """
+        cpu_vendors = (
+            'amd',
+            'apple',
+            'intel',
+            'ibm',
+        )
+        yield self.random_element(elements=cpu_vendors)
+
+
+    def onyo_directories(self,
+                         num: int = 1) -> Generator[str, None, None]:
+        r"""
+        Yield directory names.
+
+        Useful for ``onyo.path.directory``.
+        """
+        locations = (
+            'repair',
+            'shelf',
+            'warehouse',
+            'group',
+            'group/Accounting',
+            'group/Creative',
+            'group/HR',
+            'group/IT',
+            'group/Operations',
+            'group/Purchasing',
+            'group/Sales',
+        )
+        for _ in range(num):
+            yield self.random_element(elements=locations)
+
+
+    def onyo_keyboards(self,
+                       num: int = 1) -> Generator[str, None, None]:
+        r"""
+        Yield keyboard layout names.
+        """
+        keyboards = (
+            'azerty',
+            'qwerty',
+            'qwertz',
+            'qzerty',
+            'qüerty',
+            'ąžerty',
+        )
+        yield self.random_element(elements=keyboards)
+
+
+    def onyo_manufacturers(self,
+                           num: int = 1) -> Generator[str, None, None]:
+        r"""
+        Yield manufacturers.
+        """
+        manufacturers = (
+            'apple',
+            'asus',
+            'cisco',
+            'dell',
+            'eizo',
+            'framework',
+            'hp',
+            'lenovo',
+            'samsung',
+            'sun',
+            'toshiba',
+            'zebra',
+        )
+        yield self.random_element(elements=manufacturers)
+
+
+    def onyo_types(self,
+                   num: int = 1) -> Generator[str, None, None]:
+        r"""
+        Yield asset types.
+        """
+        types = (
+            'desktop',
+            'display',
+            'laptop',
+            'pdu',
+            'server',
+            'switch',
+            'ups',
+        )
+        yield self.random_element(elements=types)
+
+
+@pytest.fixture(scope="session")
+def fixture_fake() -> Generator:
+    r"""
+    Yield a faker object with the Onyo Provider loaded.
+
+    Onyo-specific functions are all prefixed with ``onyo_``.
+    """
+    from faker import Faker
+
+    _fake = Faker()
+    _fake.add_provider(OnyoProvider)
+    yield _fake
+
+
+@contextmanager
+def _context_for_fixture(val_to_yield_after_setup):
+    yield val_to_yield_after_setup
+
+
+@pytest.fixture(scope='function', name='fake')
+def fake_function_scope(fixture_fake):
+    with _context_for_fixture(fixture_fake) as result:
+        yield result
+
+
+@pytest.fixture(scope='class')
+def fake_class_scope(fixture_fake):
+    with _context_for_fixture(fixture_fake) as result:
+        yield result
+
+
+@pytest.fixture(scope='module')
+def fake_module_scope(fixture_fake):
+    with _context_for_fixture(fixture_fake) as result:
+        yield result
+
+
+@pytest.fixture(scope='session')
+def fake_session_scope(fixture_fake):
+    with _context_for_fixture(fixture_fake) as result:
+        yield result
