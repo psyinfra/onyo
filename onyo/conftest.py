67cfb8364f3a2fc0ba630218dadddba8d32d576c
Alex Waite <alex@waite.eu>
2024-11-20 12:48:42 +0100

    TST: ui marker also support classes

--- onyo/conftest.py
+++ onyo/conftest.py
@@ -252,7 +252,17 @@ def helpers() -> Type[Helpers]:
 
 
 @pytest.fixture(scope='function', autouse=True)
-def set_ui(request) -> None:
+def set_ui_function(request) -> None:
+    r"""Set up onyo.lib.ui according to a dict provided by the 'ui' marker"""
+    from onyo.lib.ui import ui
+    m = request.node.get_closest_marker('ui')
+    if m:
+        ui.set_yes(m.args[0].get('yes', False))
+        ui.set_quiet(m.args[0].get('quiet', False))
+        ui.set_debug(m.args[0].get('debug', False))
+
+@pytest.fixture(scope='class', autouse=True)
+def set_ui_class(request) -> None:
     r"""Set up onyo.lib.ui according to a dict provided by the 'ui' marker"""
     from onyo.lib.ui import ui
     m = request.node.get_closest_marker('ui')
