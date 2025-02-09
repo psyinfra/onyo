eb98c063fce27703238b42548a693f24377d5bc2
Alex Waite <alex@waite.eu>
2025-02-09 17:09:12 +0100

    DOC: uplift docstrings in consts.py

--- onyo/lib/consts.py
+++ onyo/lib/consts.py
@@ -9,37 +9,40 @@ if TYPE_CHECKING:
 
 RESERVED_KEYS = ['template', 'onyo'] + list(PSEUDOKEY_ALIASES.keys())
 # TODO: That's not right yet. We need all aliases and the "onyo." namespace
-r"""Key names that are reserved and must not be part of asset content.
+r"""Reserved key names that must not be part of asset content.
 
-These keys have functional meaning for Onyo. Thus they are reserved and cannot
-be part of asset content.
+These keys have functional meaning for Onyo. They are reserved and cannot be
+part of asset content.
 """
 
 KNOWN_REPO_VERSIONS = ['1', '2']
-r"""Onyo repository versions that this version of onyo knows.
+r"""Onyo repository versions that this onyo version knows about.
 
-Needed to realize when onyo runs on a repo that was created by a newer version.
-(Or a user messed it up).
+Use for backwards compatibility, upgrades, and detecting repositories created by
+a newer version.
 """
 
 SORT_ASCENDING = 'ascending'
 r"""Sort ascending.
 
-Used to sort the output of :py:func:`onyo_get`.
+Use to sort the output of :py:func:`onyo_get`.
 """
+
 SORT_DESCENDING = 'descending'
 r"""Sort descending.
 
-Used to sort the output of :py:func:`onyo_get`.
+Use to sort the output of :py:func:`onyo_get`.
 """
 
 UNSET_VALUE = '<unset>'
-r"""String to represent keys that are not set.
+r"""Type-symbol for unset keys.
+
+Use for type matching (:py:func:`onyo_get`) and output.
 """
 
 TYPE_SYMBOL_MAPPING = {"<dict>": (dict, UserDict),
                        "<list>": list}
 r"""Mapping of Onyo type-symbols with Python types.
 
-For use with type matching (:py:func:`onyo_get`) and output.
+Use for type matching (:py:func:`onyo_get`) and output.
 """
