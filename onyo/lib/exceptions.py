6e6d762728c116feac7a58f53ad8b27af53e52a7
Alex Waite <alex@waite.eu>
2025-02-09 17:38:22 +0100

    DOC: uplift docstrings in exceptions.py

--- onyo/lib/exceptions.py
+++ onyo/lib/exceptions.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 
 class UIInputError(Exception):
-    r"""Raised if UI failed when trying to read input"""
+    r"""Raise if UI failed when trying to read input."""
 
 
 class OnyoCLIExitCode(Exception):
-    r"""Raised if the onyo CLI should exit with a specific value."""
+    r"""Raise if the Onyo CLI should exit with a specific value."""
     def __init__(self,
                  message: str,
                  returncode: int):
@@ -15,57 +15,63 @@ class OnyoCLIExitCode(Exception):
 
 
 class OnyoRepoError(Exception):
-    r"""Raised if something is wrong with an Onyo repository."""
+    r"""Raise if something is wrong with an Onyo repository."""
 
 
 class OnyoInvalidRepoError(OnyoRepoError):
-    r"""Raised if the repository is invalid."""
+    r"""Raise if the repository is invalid."""
 
 
 class OnyoProtectedPathError(Exception):
-    r"""Raised if path is protected (.anchor, .git/, .onyo/)."""
+    r"""Raise if path is protected.
+
+    For example: ``.git/``, :py:data:`onyo.lib.onyo.OnyoRepo.ANCHOR_FILE_NAME`,
+    :py:data:`onyo.lib.onyo.OnyoRepo.ONYO_DIR`, etc.
+    """
 
 
 class OnyoInvalidFilterError(Exception):
-    r"""Raised if filters are invalidly defined."""
+    r"""Raise if filters are invalidly defined."""
 
 
 class InvalidArgumentError(Exception):
-    r"""Raised a (CLI-) command is invalidly called beyond what's covered by argparse."""
+    r"""Raise if a (CLI-) command is invalidly called beyond what's covered by argparse."""
 
 
 class InvalidAssetError(Exception):
-    r"""Raised if an asset is invalid."""
+    r"""Raise if an asset is invalid."""
 
 
 class InventoryOperationError(Exception):
-    r"""Raised if an inventory operation cannot be executed."""
+    r"""Raise if an inventory operation cannot be executed."""
 
 
 class InvalidInventoryOperationError(InventoryOperationError):
-    r"""Raised if an invalid inventory operation is requested."""
+    r"""Raise if an invalid inventory operation is requested."""
 
 
 class InventoryDirNotEmpty(InvalidInventoryOperationError):
-    r"""Raised if an inventory directory needs to be empty to perform an operation but is not."""
+    r"""Raise if an inventory directory is not empty but needs to be in order to perform an operation."""
 
 
 class PendingInventoryOperationError(InventoryOperationError):
-    r"""Raised if there are unexpected pending operations."""
+    r"""Raise if there are unexpected pending operations."""
     # TODO  -> enhance message w/ hint to Inventory.reset/commit?
     #          would be useful in python context only
 
 
 class NoopError(InventoryOperationError):
-    r"""Raised if a requested operation is a Noop."""
-    # This is intended to signal that an inventory operation would not result in any change, so that callers can decide
-    # on their failure paradigm:
-    # "Result oriented already-fine-no-failure" vs "Task oriented can't-do-failure".
+    r"""Raise if a requested operation is a no-op.
+
+    Signal that an inventory operation would not result in a change, allowing
+    callers to determine the failure paradigm: state (already-fine; success) vs
+    task (can't-do; failure).
+    """
 
 
 class NotAnAssetError(Exception):
-    r"""Raised if an object is expected to be an asset but is not."""
+    r"""Raise if an object is expected to be an asset but is not."""
 
 
 class NotADirError(Exception):
-    r"""Raised if an object is expected to be a directory but is not."""
+    r"""Raise if an object is expected to be a directory but is not."""
