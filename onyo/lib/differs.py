47bad87e200cf996004859427bf70b52bb869bc2
Alex Waite <alex@waite.eu>
2025-02-15 10:28:37 +0100

    remove unused, dead code from differs.py

--- onyo/lib/differs.py
+++ onyo/lib/differs.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
 from difflib import unified_diff
-from functools import partial
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 from onyo.lib.onyo import OnyoRepo
 from onyo.lib.utils import dict_to_asset_yaml
 
 if TYPE_CHECKING:
+    from pathlib import Path
     from typing import Generator
 
 # Differs signature: (repo: OnyoRepo, operands: tuple) -> Generator[str, None, None]:
@@ -29,16 +28,8 @@ def diff_path_change(src: Path, dst: Path) -> Generator[str, None, None]:
     yield f"{str(src)} -> {str(dst)}"
 
 
-diff_new_asset = partial(diff_assets, asset_old={})
-diff_rm_asset = partial(diff_assets, asset_new={})
-diff_modified_asset = diff_assets
-diff_renamed_asset = diff_assets  # This is the same, because a rename requires a change in keys composing the name (or change in config).
 
 
-def diff_moved_asset(asset_old: dict | Path, asset_new: Path) -> Generator[str, None, None]:
-    # could be same. Just check isinstance?
-    yield from diff_path_change(asset_old if isinstance(asset_old, Path) else asset_old.get('onyo.path.absolute'),
-                                asset_new)
 
 
 def differ_new_assets(repo: OnyoRepo, operands: tuple) -> Generator[str, None, None]:
